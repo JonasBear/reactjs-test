4109cb533df17ff88459a416e3f6b5fe6a0d45c2
unknown <namesjonas@gmail.com>
2024-06-08 20:28:10 +0330

    css styling:inline

---
+++ src/Button.jsx
@@ -0,0 +1,16 @@
+
+function Button(){
+    const styles = {
+        backgroundColor: "hsla(214, 78%, 62%, 0.696)",
+        color: "white",
+        padding: "10px 20px",
+        borderRadius: "7px",
+        border: "none",
+        cursor: "pointer",
+    }
+    return(
+        <button style={styles}>Intrested?</button>
+    );
+}
+
+export default Button
--- src/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Button.module.css' 
-
-function Button(){
-    return(
-        <button className={styles.button}>Intrested?</button>
-    );
-}
-
-export default Button
